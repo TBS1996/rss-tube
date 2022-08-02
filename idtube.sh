6ec5066353511d2da1ce66c563cc7ed20d10e225
markbojack <hoggel_tabke@slmail.me>
2022-08-02 05:47:28 -0400

    use yewtu.be

--- idtube.sh
+++ idtube.sh
@@ -1,35 +1,18 @@
-#! /bin/bash
+#!/bin/bash
 
-SEARCH=$(sed 's/ /+/' <<< $1)
-pre=$"https://www.youtube.com/feeds/videos.xml?channel_id="
-HTML=$(curl -s "https://www.youtube.com/results?search_query=$SEARCH&sp=EgIQAg%253D%253D")
+search_term=$(sed 's/ /+/g' <<< "$1")
+html=$(curl -s https://yewtu.be/search?q="$search_term")
+channel_info=$(echo "$html" | sed '/<a href="\/channel\//,$!d' | head -n 10)
+channel_id=$(echo "$channel_info" | grep channel | awk -F"/" '{print $NF}' | sed 's/..$//')
+channel_name=$(echo "$channel_info" | grep auto | cut -d ">" -f2 | cut -d "&" -f1)
 
+yt_url="https://www.youtube.com/feeds/videos.xml?channel_id="
+insert_text=$"$yt_url$channel_id \"~"$channel_name"\" YouTube"
+conf_file="$HOME/.config/newsboat/urls"
 
-
-# grabs the channel id needed for the rss feed
-if [[ $HTML =~ (channelId)\":.{25} ]]; then
-  ID=$"'${BASH_REMATCH[0]}'"
-fi
-ID=${ID:13:24}
-RSS_URL="$pre$ID"
-XML=$(curl -s $RSS_URL)
-
-
-# gets the name of the channel, I hope to find a better way...
-if [[ $XML =~ name.{25} ]]; then
-  ID=$"'${BASH_REMATCH[0]:5}'"
-fi
-IFS='/'; NAME=($ID); unset IFS;
-NAME="${NAME::-1}"
-NAME="${NAME:1}"
-
-
-
-INSERT=$"$RSS_URL Youtube \"~$NAME\""
-
-FILE=~/.newsboat/urls
-if test -f "$FILE"; then
-    echo $INSERT >> $FILE
-    echo "inserted $NAME's  youtube channel to  $FILE."
+if [ -f "$conf_file" ]; then
+  echo "$insert_text" >> "$conf_file" && echo "Appended $conf_file"
+else
+  echo "Please create $conf_file"
 fi
 
