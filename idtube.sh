e89b1a1948a9990f34b067f9f7d3280430971f80
tor <torberge@outlook.com>
2022-08-02 14:33:22 +0200

    incorporate tips from /u/bri-an

--- idtube.sh
+++ idtube.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 search_term=$(sed 's/ /+/g' <<< "$1")
 html=$(curl -s https://yewtu.be/search?q="$search_term")
 
-channel_info=$(echo "$html" | sed '/<a href="\/channel\//,$!d' | head -n 10)
+channel_info=$(echo "$html" | grep -A 6 '<a href="/channel/' | head -n 7)
 channel_id=$(echo "$channel_info" | grep channel | awk -F"/" '{print $NF}' | sed 's/..$//')
 channel_name=$(echo "$channel_info" | grep auto | cut -d ">" -f2 | cut -d "&" -f1)
 
@@ -14,9 +14,9 @@ conf_file="$HOME/.newsboat/urls"
 xdf_path="$HOME/.config/newsboat/urls" 
 
 
-if [ -f "$conf_file" ]; then
+if [[ -f "$conf_file" ]]; then
   echo "$insert_text" >> "$conf_file" && echo "Appended $channel_name to  $conf_file"
-elif [ -f "$xdf_path" ]; then
+elif [[ -f "$xdf_path" ]]; then
   echo "$insert_text" >> "$xdf_path"  && echo "Appended $channel_name to  $xdf_path"
 else
   echo "Please create $conf_file"
