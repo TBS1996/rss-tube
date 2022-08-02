9854bc442060c112a23e3d57d68bc86ed73674b3
tor <torberge@outlook.com>
2022-08-02 13:38:29 +0200

    accept causality-loop's request and add support for two config paths

--- idtube.sh
+++ idtube.sh
@@ -2,16 +2,22 @@
 
 search_term=$(sed 's/ /+/g' <<< "$1")
 html=$(curl -s https://yewtu.be/search?q="$search_term")
+
 channel_info=$(echo "$html" | sed '/<a href="\/channel\//,$!d' | head -n 10)
 channel_id=$(echo "$channel_info" | grep channel | awk -F"/" '{print $NF}' | sed 's/..$//')
 channel_name=$(echo "$channel_info" | grep auto | cut -d ">" -f2 | cut -d "&" -f1)
 
 yt_url="https://www.youtube.com/feeds/videos.xml?channel_id="
-insert_text=$"$yt_url$channel_id \"~"$channel_name"\" YouTube"
-conf_file="$HOME/.config/newsboat/urls"
+insert_text=$"$yt_url$channel_id Youtube \"~"$channel_name"\""
+
+conf_file="$HOME/.newsboat/urls"
+xdf_path="$HOME/.config/newsboat/urls" 
+
 
 if [ -f "$conf_file" ]; then
-  echo "$insert_text" >> "$conf_file" && echo "Appended $conf_file"
+  echo "$insert_text" >> "$conf_file" && echo "Appended $channel_name to  $conf_file"
+elif [ -f "$xdf_path" ]; then
+  echo "$insert_text" >> "$xdf_path"  && echo "Appended $channel_name to  $xdf_path"
 else
   echo "Please create $conf_file"
 fi
